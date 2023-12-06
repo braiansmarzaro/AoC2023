8e04d9bd604b9d86cad2588d4614465a11da2338
Deivid Braian <deividbraian5@gmail.com>
2023-12-06 09:41:27 -0300

    complete day 3

--- day03/day03.py
+++ day03/day03.py
@@ -61,14 +61,87 @@ def check_numbers(index, pos, mat=data):
 
 
 # Para cada elemento que não é ponto ou número, olha em volta se tem números
-total = 0
-for index, line in enumerate(data):
-    for pos, character in enumerate(line):
-        if character == '.' or character.isdigit():
-            continue
-        
-        total += check_numbers(index, pos)
+def part1():
+    total = 0
+    for index, line in enumerate(data):
+        for pos, character in enumerate(line):
+            if character == '.' or character.isdigit():
+                continue
+            
+            total += check_numbers(index, pos)
 
-r, l =22,12
-print(total)
+    r, l =22,12
+    print(total)
 
+# Part 2
+
+def part2():
+    
+    def check_numbers(index, pos, mat=data):
+        seen_digit:list[int,int] = []
+
+        """Check surrounding numbers around the signal
+
+        Args:
+            index (_type_): _description_
+            pos (_type_): _description_
+            mat (_type_, optional): _description_. Defaults to data.
+        """
+        def get_number(row, col):
+            """From a found digit, get the whole number"""
+            number = []
+            i = 1
+            predecessor = mat[row][col-i]
+            while predecessor.isdigit() and [row,col-i] not in seen_digit:
+                number.insert(0, predecessor)
+                seen_digit.append([row,col-i])
+                i += 1
+                predecessor = mat[row][col-i]
+
+            if [row,col] not in seen_digit:
+                number.append(mat[row][col])
+                seen_digit.append([row,col])
+
+            i = 1
+            sucessor = mat[row][col+i]
+            while sucessor.isdigit() and [row,col+i] not in seen_digit:
+                number.append(sucessor)
+                seen_digit.append([row,col+i])
+
+                i += 1
+                if col+i >= len(mat[0]):
+                    break
+                sucessor = mat[row][col+i]
+            if number:
+                return int(''.join(number))
+            return None
+
+        numbers = []
+        for row in range(index-1, index+2):
+            for col in range(pos-1, pos+2):
+                if row == index and col == pos:
+                    continue
+                if row >= len(mat) or col >= len(mat[0]):
+                    continue
+
+                if mat[row][col].isdigit():
+                    num = get_number(row, col)
+                    if num is not None:
+                        numbers.append(num)
+                    #print(mat[row][col])
+        print(numbers)
+        if len(numbers) == 2:
+            return numbers[0]*numbers[1]
+        return 0
+
+
+    total = 0
+    for index, line in enumerate(data):
+        for pos, character in enumerate(line):
+            if character == '*':
+                
+                total += check_numbers(index, pos)
+
+    print(total)
+
+part2()
